bbb32a9a6095a1bb75d2d5c89b98b18a47f12d47
ssy <1974193036@qq.com>
2025-07-29 23:23:58 +0800

    feat: 入口文件-createFiber

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    react: true,
+    typescript: false,
+  },
+  {
+    rules: {
+      'no-console': 'off',
+      'perfectionist/sort-imports': 'off',
+      'perfectionist/sort-exports': 'off',
+      'perfectionist/sort-named-exports': 'off',
+    },
+  },
+)
